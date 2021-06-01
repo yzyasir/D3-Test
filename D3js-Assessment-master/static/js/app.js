815c8ea9b5710c3c0bef7229b472ab27f89535e6
Yasir Zafar <yzyasir@yahoo.com>
2021-05-31 23:02:56 -0400

    got bar chart made, time to work on scales to make it look better

--- D3js-Assessment-master/static/js/app.js
+++ D3js-Assessment-master/static/js/app.js
@@ -11,7 +11,10 @@ d3.json('data/data.json').then(function(data){ // Here getting the data using an
       height = 600,
       svgHeight,
       svgWidth,
-      svg
+      svg, // svg(scalable vector graphics)
+      X,
+      Y,
+      barChart
 
 
 // _______________________________________________________________________________
@@ -24,17 +27,17 @@ d3.json('data/data.json').then(function(data){ // Here getting the data using an
 // _______________________________________________________________________________
 
 // Basic parameters of the svg (part 2)
-  margin = {
+  margin = { 
     top: 15,
-    bottom: 15,
-    left: 70,
-    right: 15
+    bottom: 15, // These margins will be used to inset the ranges of our scales
+    left: 70, // Rather than extending from 0 to the full width and height of the chart...
+    right: 15 // ...the starts and ends of the ranges are moved inward by the corresponding margins.
   };
 
-  svgHeight = height + margin.top + margin.bottom;
-  svgWidth = width + margin.left + margin.right;
+  svgHeight = height + margin.top + margin.bottom; // the y range
+  svgWidth = width + margin.left + margin.right;  // the x range
 
-  svg = d3.select('#svg-area')
+  svg = d3.select('#svg-area') // here svg-area is being passed to svg
           .append('svg')
           .attr('width', svgWidth) // need to give the svg the attributes of height and width of whats defined above
           .attr('height', svgHeight)
@@ -45,11 +48,17 @@ d3.json('data/data.json').then(function(data){ // Here getting the data using an
               .attr('opacity', '0');
 
 // Set scales
+  // X = d3.scaleBand() // this function in d3 used to construct a new band, horizontal positions of the bars is given by a band scale
+  //       .domain(d3.range(data.length))
+  //       .range([margin.left, width - margin.right])
+  //       .padding(0.1)
 
-
-
+  // Y = d3.scaleLinear()
+  //       .domain([0, 100])
+  //       .range([height - margin.bottom, margin.top])
 
 // Create axes
+
 
 
 
@@ -60,11 +69,27 @@ d3.json('data/data.json').then(function(data){ // Here getting the data using an
 
 
 // Create Bars or Line function
-
-
-
+  var barWidth;
+  barWidth = (svgWidth / barData.length);
+  
+  barChart = svg.selectAll("rect") // bars are nothing more than rectangless, so we select all rectangles
+    .data(barData) // passed in our bar data, this method takes our data into a waiting state
+    .enter() // this method will take our data away form the waiting state and perform further methods/operations for each individual data item
+    .append("rect") // for each data item we are appending a rect in our svg(svg area) 
+    .attr("y", function(d) { // this function gets the data in its parameter
+        return svgHeight - d // attr y is then calculated when height is then subtracted by the data in the parameter
+    })
+    .attr("height", function(d) { // height is just the value of the data
+      return d;
+    })
+    .attr("width", barWidth)
+    .attr("transform", function(d, i) {
+      var translate = [barWidth * i, 0]; // this is [x, y]
+      return "translate("+ translate +")"; // in this step we calc the translation of our bars
+    })
 
 // Create Circle group
+
 
 
 
